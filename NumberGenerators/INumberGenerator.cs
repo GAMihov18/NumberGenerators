7f4e311502079db2155c7abe2b2f475b22b1ff84
Georgi Mihov <georgi.mihov04@gmail.com>
2026-04-27 22:15:53 +0200

    Initial version of INumberGenerator.cs
    Signed-off-by: Georgi Mihov <georgi.mihov04@gmail.com>

---
+++ NumberGenerators/INumberGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace NumberGenerators;
+
+public interface INumberGenerator<out T> where T : INumber<T>
+{
+    public void Initialize();
+    public IEnumerable<T> GenerateNumbers();
+    public T NextNumber { get; }
+}
