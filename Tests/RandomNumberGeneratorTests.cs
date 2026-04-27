a1004c8ec868d754653dd13b2aa021c2788a7fd7
Georgi Mihov <georgi.mihov04@gmail.com>
2026-04-28 01:37:34 +0200

    Add unit tests
    Signed-off-by: Georgi Mihov <georgi.mihov04@gmail.com>

---
+++ Tests/RandomNumberGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿using NumberGenerators;
+
+
+namespace Tests;
+public class RandomNumberGeneratorTests
+{
+	[Fact]
+	public void DifferentRandomGenerators_ShouldReturnSameValues_WhenSameSeed()
+	{
+		var gen1 = new RandomGenerator<int>(10, 20, seed: 42);
+		var gen2 = new RandomGenerator<int>(10, 20, seed: 42);
+
+		for (int i = 0; i < 100; i++)
+		{
+			Assert.Equal(gen1.NextNumber, gen2.NextNumber);
+		}
+	}
+	[Fact]
+	public void DifferentRandomGenerator_WithDifferentSeeds_ReturnsDifferentSequence()
+	{
+		var gen1 = new RandomGenerator<int>(10, 20, seed: 1);
+		var gen2 = new RandomGenerator<int>(10, 20, seed: 2);
+
+		var seq1 = Enumerable.Range(0, 20).Select(_ => gen1.NextNumber).ToArray();
+		var seq2 = Enumerable.Range(0, 20).Select(_ => gen2.NextNumber).ToArray();
+
+		Assert.NotEqual(seq1, seq2);
+	}
+}
